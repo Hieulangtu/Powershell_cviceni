c3f98b57e637cd04e5ff6fe1bb1f65435d7cde90
Hieulangtu <trunghieunguyen1905mu@gmail.com>
2024-01-17 01:03:12 +0100

    .

--- uloha16a.ps1
+++ uloha16a.ps1
@@ -1,7 +1,7 @@
 # informace
 $credentials = Get-Credential #lấy thông tin
-$smtpServer = "smtp.gmail.com"
-$smtpFrom = "trunghieunguyen1905mu@gmail.com"
+$smtpServer = "smtp.unob.cz"
+$smtpFrom = "trunghieu.nguyen@unob.cz"
 $smtpTo = "jirsamilan@gmail.com"
 $messageSubject = "Testovaci email - Uloha 16"
 $messageBody = " uloha 16"
@@ -13,8 +13,8 @@ $emailParams = @{
     To = $smtpTo
     Subject = $messageSubject
     Body = $messageBody
-    Port = 587
-    UseSsl = $true
+    Port = "587"
+    # UseSsl = $true
     Credential = $credentials
 }
 
