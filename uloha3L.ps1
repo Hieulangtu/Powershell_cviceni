046975ef2c2db7ce1c4dc888bb62036b8444db51
Hieulangtu <trunghieunguyen1905mu@gmail.com>
2024-01-17 23:14:52 +0100

    .

--- uloha3L.ps1
+++ uloha3L.ps1
@@ -2,6 +2,7 @@
 $s = "506f7765727368656c6c20697320617765736f6d6521" 
 #Rozdělení hexadecimálního řetězce na dvojice znaků 
 $hexPairs = $s -split '(..)' | Where-Object { $_ } 
+Write-Output $hexPairs
 #Převod každé dvojice na ASCII hodnotu a spojení do řetězce
 $asciiString = ($hexPairs | ForEach-Object { [char]([Convert]::ToInt32($_, 16)) }) -join ''
 #Vypsání výsledku
